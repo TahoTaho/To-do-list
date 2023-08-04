87423253f4db36491c8b7250973e3bd57a846aca
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-08-04 15:07:25 +0800

    Added task description, task done button, and Remove task button

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ addTaskButton.addEventListener('click', newTask);
 function newTask() {
     const task = document.createElement('div');
     task.className = 'task';
+
+    const taskDescription = document.createElement('p');
+    taskDescription.className = 'task-description';
+    taskDescription.textContent = 'Jojos';
+
+    const taskDoneButton = document.createElement('button');
+    taskDoneButton.className = 'task-done';
+    taskDoneButton.textContent = 'Done';
+
+    const removeTaskButton = document.createElement('button');
+    removeTaskButton.className = 'remove-task';
+    removeTaskButton.textContent = 'Remove';
+
+    task.append(taskDescription);
+    task.append(taskDoneButton);
+    task.append(removeTaskButton);
     taskContainer.append(task);
 }
