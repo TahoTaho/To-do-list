f73b8b39b35f1c27b2dc27713d151f4323252526
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-08-04 19:20:38 +0800

    Input field value will now be displayed after add task button is click. If input field is empty it will do nothing

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const addTaskButton = document.getElementById('add-task');
 const taskContainer = document.querySelector('div.task-container');
+const inputField = document.querySelector('input.text-field');
+
 addTaskButton.addEventListener('click', newTask);
 
 function newTask() {
-    const task = document.createElement('div');
-    task.className = 'task';
+    console.log(inputField.value);
+    if (inputField.value != '') {
+        const task = document.createElement('div');
+        task.className = 'task';
+    
+        const taskDescription = document.createElement('p');
+        taskDescription.className = 'task-description';
+        taskDescription.textContent = inputField.value;
+        
+        const taskDoneButton = document.createElement('button');
+        taskDoneButton.className = 'task-done';
+        taskDoneButton.textContent = 'Done';
 
-    const taskDescription = document.createElement('p');
-    taskDescription.className = 'task-description';
-    taskDescription.textContent = 'Jojos';
+        const removeTaskButton = document.createElement('button');
+        removeTaskButton.className = 'remove-task';
+        removeTaskButton.textContent = 'Remove';
 
-    const taskDoneButton = document.createElement('button');
-    taskDoneButton.className = 'task-done';
-    taskDoneButton.textContent = 'Done';
-
-    const removeTaskButton = document.createElement('button');
-    removeTaskButton.className = 'remove-task';
-    removeTaskButton.textContent = 'Remove';
-
-    task.append(taskDescription);
-    task.append(taskDoneButton);
-    task.append(removeTaskButton);
-    taskContainer.append(task);
+        inputField.value = '';
+        
+        task.append(taskDescription);
+        task.append(taskDoneButton);
+        task.append(removeTaskButton);
+        taskContainer.append(task);
+    }
+    else {
+        return console.log('Input field is empty');
+    }
+    
 }
